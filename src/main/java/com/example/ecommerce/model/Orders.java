4033eeb2fcf24a7386a53cfdd3e8390cc3b3d075
MSAbishek <subbuabishek@gmail.com>
2025-04-11 09:57:03 +0530

    changes

--- src/main/java/com/example/ecommerce/model/Orders.java
+++ src/main/java/com/example/ecommerce/model/Orders.java
@@ -1,6 +1,5 @@
 package com.example.ecommerce.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
